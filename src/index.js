07635123aee48c643c9711b43287633f288c674e
Shivangi541 <shivangidutta68@gmail.com>
2025-07-11 23:31:16 +0530

    setup nodemon and modified routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => {
-  res.send("Hello, World!");
-}); // request handler
+
+app.use("/test", (req, res) => {
+  res.send("Test route is working!");
+});
+// app.use("/", (req, res) => {
+//   res.send("Hello, World!");
+// }); // request handler
 app.listen(3000, () => {
   console.log("Server is successfully running on port 3000");
 });
