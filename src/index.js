2010fb8760ebcf87c1f9a461ebf7553fef4f53d9
Shivangi541 <shivangidutta68@gmail.com>
2025-07-15 00:04:40 +0530

    created a mock signup api and tried fetching data from the user model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/admin_userAuth"); // adjust path if needed
-const connectDB = require("./config/database"); // Ensure database connection is established
+const { adminAuth, userAuth } = require("./middlewares/admin_userAuth");
+const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user"); // Import the User model
-// creating post api
+const User = require("./models/user");
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Sarbari",
-    lastName: "Dutta",
-    email: "dutta69sarbari@gmail.com",
-    password: "sar123",
-    age: 50,
-    gender: "Female",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.status(201).send("User created successfully");
@@ -20,7 +14,23 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error creating user: " + error.message);
   }
 });
-connectDB() // Call the connectDB function to establish a connection
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.find({ email: userEmail });
+    if (!user || user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+      console.log("User fetched successfully:", user);
+    }
+  } catch (error) {
+    res.status(400).send("Error fetching user: ", error.message);
+  }
+});
+// feed api get api
+app.get("/feed", async (req, res) => {});
+connectDB()
   .then(() => {
     console.log("Database connected successfully to 'devtinder'");
     app.listen(3000, () =>
