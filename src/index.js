3cb1bdf51d0695dec06d301622cd0ad590ec7c5c
Shivangi541 <shivangidutta68@gmail.com>
2025-07-14 21:10:40 +0530

    craeted dummy document

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-// src/index.js
 const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/admin_userAuth"); // adjust path if needed
 const connectDB = require("./config/database"); // Ensure database connection is established
 const app = express();
-
-connectDB()
+const User = require("./models/user"); // Import the User model
+// creating post api
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Sarbari",
+    lastName: "Dutta",
+    email: "dutta69sarbari@gmail.com",
+    password: "sar123",
+    age: 50,
+    gender: "Female",
+  });
+  try {
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status(400).send("Error creating user: " + error.message);
+  }
+});
+connectDB() // Call the connectDB function to establish a connection
   .then(() => {
     console.log("Database connected successfully to 'devtinder'");
     app.listen(3000, () =>
