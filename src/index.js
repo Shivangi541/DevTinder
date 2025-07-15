66c01e838c0bdfe15ca4cb1951329b1580c0538a
Shivangi541 <shivangidutta68@gmail.com>
2025-07-15 12:02:58 +0530

    created delete api

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.post("/signup", async (req, res) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.email;
   try {
-    const user = await User.find({ email: userEmail });
+    const user = await User.findOne({ email: userEmail });
     if (!user || user.length === 0) {
       return res.status(404).send("User not found");
     } else {
@@ -29,7 +29,47 @@ app.get("/user", async (req, res) => {
   }
 });
 // feed api get api
-app.get("/feed", async (req, res) => {});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+    console.log("Feed fetched successfully:", users);
+  } catch (error) {
+    res.status(400).send("Error fetching feed: ", error.message);
+  }
+});
+// demo for findById()
+app.get("/userbyID/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+      console.log("User fetched successfully by ID:", user);
+    }
+  } catch (error) {
+    res.status(400).send("Error fetching user by ID: ", error.message);
+  }
+});
+//delete api
+app.delete("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.findOneAndDelete({ email: userEmail });
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send("User deleted successfully");
+      console.log("User deleted successfully:", user);
+    }
+  } catch (error) {
+    res.status(400).send("Error deleting user: " + error.message);
+  }
+});
+//update api
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully to 'devtinder'");
